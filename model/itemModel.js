e87d9062b4c095b5de74d4a98c08568fe32fa8d3
unknown <nasifjuhayer@gmail.com>
2025-01-31 20:26:00 -0500

    init

---
+++ model/itemModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    rating: { type: Number, required: true },
+    status: { type: String, required: true },
+    cover: { type: String, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
+
+const models = {}; // Store models to avoid redefining
+
+export const getItemModel = (category) => {
+  if (!models[category]) {
+    models[category] = mongoose.model(category, itemSchema);
+  }
+  return models[category];
+};
